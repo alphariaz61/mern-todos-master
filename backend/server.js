5c23dcfa939665a5f9ec38564f377c438b2446d9
shaum316 <shaum316@gmail.com>
2022-09-07 06:53:37 -0700

    added another backend script

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const app = express()
 require("colors")
 require("dotenv").config({ path : "backend/config.env" })
 const connectDB = require("./scripts/database")
+const setStatic = require("./scripts/setStatic")
 
 // Body Parser Middleware
 app.use(express.json())
@@ -13,10 +14,7 @@ app.use("/api/users", require("./routes/usersRoute"))
 app.use("/api/todos", require("./routes/todosRoute"))
 
 // Server Client App
-const root = __dirname.replace("/backend", "")
-const build = `${root}/client/build`
-app.use(express.static(build))
-app.get("*", (_, res) => res.sendFile(`${build}/index.html`))
+setStatic(app, express)
 
 // Start Server
 app.listen(process.env.PORT || 5000, () => {

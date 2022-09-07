144d6a3248573ad3280774923464ba4f246d3d46
shaum316 <shaum316@gmail.com>
2022-09-06 21:06:20 -0700

    Added features to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import LogoutButton from "./LogoutButton";
 
 export default function Footer () {
+    const year = (new Date()).getFullYear()
     return (
         <footer id="footer" className="d-flex justify-content-between align-items-center px-5 py-3 ">
-            <span className="text-muted">© 2022 Alpha Javascript Inc</span>
+            <span className="text-muted">© {year} Alpha Javascript Inc</span>
             <LogoutButton/>
         </footer>
     )
